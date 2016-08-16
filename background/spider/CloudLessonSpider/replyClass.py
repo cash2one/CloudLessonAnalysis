be18c5ce54b9aa6b2de3b2f9620b984169e2e210
lyn716 <tonylu716>
2016-08-17 04:58:32 +0800

    add method of replyClass

--- background/spider/CloudLessonSpider/replyClass.py
+++ background/spider/CloudLessonSpider/replyClass.py
@@ -7,8 +7,12 @@
 @editor:    PyCharm
 @create:    2016-08-14 3:28
 @description:
-            本类用于评论信息的解析写入，大部分属于write属性
+            本类用于post下的reply信息的解析写入，大部分属于write属性
+            进入形如/learn/ZJU-1000004001?tid=1000005000#/learn/forumdetail?pid=1000215022
+            这样的post详情页面，找reply
 """
+from LessonDataSpider import *
+from userClass import User
 
 class Reply:
     def __init__(self,replyEle,conn=None):
@@ -18,32 +22,46 @@ class Reply:
             self.cur = self.conn.cursor()
 
     @property
-    def content(self):
+    def is_anony(self):
         return
 
     @property
     def content(self):
+        return self.replyEle.find_element_by_class_name('f-richEditorText').text
+
+    @property
+    def date(self):
         return
 
     @property
-    def content(self):
+    def author(self):
         return
 
     @property
-    def content(self):
+    def vote_box(self):
+        return self.replyEle.find_element_by_class_name('votebox')
+
+    @property
+    def like_cot(self):
         return
 
     @property
-    def content(self):
+    def unlike_cot(self):
         return
 
     @property
-    def content(self):
-        return
-
-    @property
-    def content(self):
-        return
+    def comment_cot(self):
+        return self.replyEle.find_element_by_class_name('cmtBtn').text
 
     def save_to_db(self):
-        pass
+        if self.is_anony:
+            pass
+        else:
+            pass
+
+
+if __name__=='__main__':
+    lds = LessonDataSpider(qq_login=True)
+    post_ids = lds.get_post_info_by_db()[:3]
+    for post_id in post_ids:
+        lds.get_reply_by_crawling(post_id)
